369a82efc1c231a167a7bc886bfac94ff4e11d28
Inés Fajardo Romero <finesjanellys@hotmail.es>
2024-07-16 16:26:07 -0500

    containescard

--- src/App.js
+++ src/App.js
@@ -11,17 +11,56 @@ function App() {
       
       <div className="content">
         <BarnerComponent/>
+        <div className="section_card">
         <div className='card_content'>
-          <CardComponent />
-          <CardComponent />
-          <CardComponent />
-          <CardComponent />
-          <CardComponent />
-          <CardComponent />
-          <CardComponent />
+            <div class="card_container">
+                <img class="img_videos" src="/img/frontend1.jpeg" alt="frontend1" />
+                <div class="text_descripcion">
+                    <div class="buttons_card">
+                        <p><img class="img_trash" src="/img/eliminar.svg"  alt="eliminar" /> <span>Eliminar</span></p>
+                        <p><img class="img_trash" src="/img/editar.svg"  alt="editar" /><span>Editar</span></p>
+                    </div>
+                </div>
+            </div>
+            <div class="card_container">
+                <img class="img_videos" src="/img/frontend1.jpeg" alt="frontend1" />
+                <div class="text_descripcion">
+                    <div class="buttons_card">
+                        <p><img class="img_trash" src="/img/eliminar.svg"  alt="eliminar" /> <span>Eliminar</span></p>
+                        <p><img class="img_trash" src="/img/editar.svg"  alt="editar" /><span>Editar</span></p>
+                    </div>
+                </div>
+            </div>
+            <div class="card_container">
+                <img class="img_videos" src="/img/frontend1.jpeg" alt="frontend1" />
+                <div class="text_descripcion">
+                    <div class="buttons_card">
+                        <p><img class="img_trash" src="/img/eliminar.svg"  alt="eliminar" /> <span>Eliminar</span></p>
+                        <p><img class="img_trash" src="/img/editar.svg"  alt="editar" /><span>Editar</span></p>
+                    </div>
+                </div>
+            </div>
+            <div class="card_container">
+                <img class="img_videos" src="/img/frontend1.jpeg" alt="frontend1" />
+                <div class="text_descripcion">
+                    <div class="buttons_card">
+                        <p><img class="img_trash" src="/img/eliminar.svg"  alt="eliminar" /> <span>Eliminar</span></p>
+                        <p><img class="img_trash" src="/img/editar.svg"  alt="editar" /><span>Editar</span></p>
+                    </div>
+                </div>
+            </div>
+            <div class="card_container">
+                <img class="img_videos" src="/img/frontend1.jpeg" alt="frontend1" />
+                <div class="text_descripcion">
+                    <div class="buttons_card">
+                        <p><img class="img_trash" src="/img/eliminar.svg"  alt="eliminar" /> <span className='text_boton'>Eliminar</span></p>
+                        <p><img class="img_trash" src="/img/editar.svg"  alt="editar" /><span className='text_boton'>Editar</span></p>
+                    </div>
+                </div>
+            </div>
         </div>
 
-
+        </div>
       </div>
       <FooterComponent/>
     </div>
